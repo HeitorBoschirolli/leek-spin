014f8a076b86fbd5441e014994519a9a0907c394
Heitor Boschirolli <heitor.boschirolli@gmail.com>
2020-10-18 11:49:25 -0300

    Add audio

--- src/components/Loop/index.js
+++ src/components/Loop/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import gif from '../../assets/gifs/leek-spin.gif'
+import leekSpinAudio from '../../assets/audio/leek-spin.mp3';
 import { FullWidthImage } from './styled';
 
 const Loop = () => {
+    const audio = new Audio(leekSpinAudio);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.loop = true;
+    audio.play();
     return <FullWidthImage src={gif}/>;
-}
+};
 
 export default Loop;
